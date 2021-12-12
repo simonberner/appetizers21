0fccccdb64ede61b14375895e55dcbbada8689f9
Simon Berner <sibebzh@gmail.com>
2021-12-12 22:05:17 +0100

    refactor(AsyncImage): :hammer: refactor AppetizerListCell
    Refactor the AppetizerListCell to use the new AsyncImage instead
    AppetizerRemoteImage

--- Appetizers21/Views/CustomModifiers.swift
+++ Appetizers21/Views/CustomModifiers.swift
@@ -17,6 +17,16 @@ struct StandardButtonStyle: ViewModifier {
     }
 }
 
+struct ListCellImageStyle: ViewModifier {
+
+    func body(content: Content) -> some View {
+        content
+            .aspectRatio(contentMode: .fit)
+            .frame(width: 120, height: 90)
+            .cornerRadius(8)
+    }
+}
+
 //extension View {
 //    func standardButtonStyle() -> some View {
 //        self.modifier(StandardButtonStyle())
